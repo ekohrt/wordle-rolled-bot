b32557083015eaf7c749a4904da943aaeb8c193d
Ethan Kohrt <e.a.kohrt@gmail.com>
2022-07-14 01:46:05 -0400

    made tracery bot

--- main.py
+++ main.py
@@ -17,15 +17,73 @@ Created on Wed Jul 13 13:22:55 2022
 
 
 import discord
-import random
 import re
-import time
 import os
 
 TOKEN = os.environ['DISCORD_TOKEN']
 CHANNEL_NAME = 'general'
 
 client = discord.Client()
+
+import tracery
+from tracery.modifiers import base_english
+
+# put your grammar here as the value assigned to "rules"
+rules = {
+    # a lip phrase is two parts: 
+    # PART1: Noun + ING-Verb, or ADJ/[some_nouns]-lipped)
+    # PART2: Noun + ER-verb
+    # or a 
+    "lip_phrase": ["#lip_part_1# #lip_part_2#"],
+    "lip_part_1": ["#lip_noun# #lip_verb_ing#", "#lipped_prefix#-lipped"],
+    "lip_part_2": ["#lip_noun# #lip_verb_er#"],
+    "lip_noun": ["lip", "lip", "liver", "lint", 'lard', "ligament", "libertarian", "ligma", "librarian", 
+                 "liquorice", "liquid", "lizard", "liquor", "lichen", "lipid", "leg", 
+                 "limb", "lid", "litter", "lettuce", "laundry", "laser", "quiver"],
+    "lip_adj": ["limp", "limp", "linear", "lithospheric", "liquidy", "limber",
+                "lippy", "limy", "lit", "literal", "little", "lanky", "loopy", "quick"],
+    "lip_verb_ing": ["licking", "licking", "lipping", "flicking", "listing", "loving"],
+    "lip_verb_er": ["licker", "licker", "lipper", "lifter", "flipper", "slipper", 
+                    "lover", "liquefier"],
+    "lipped_prefix": ["#lip_adj#", "lip_noun"],
+    
+    "rolled": ["rolled like #roll_thing_singular##modifier#.", "rolled harder than #roll_thing-singular##modifier#."],
+    "modifier": ["", " on #thing_on#", " in #thing_in#", " covered in #roll_thing_plural#"],
+    "roll_thing_singular": ["a cinnamon roll", "a fruit roll-up", "a rolling pin", 
+                            "a tyrannosrollus rex", "a bean burrito", "a Ferrari#wheel_modifier#"
+                            "a pizza roll", "a sushi roll", "a spring roll", "a tootsie roll", 
+                            "a pinewood derby car#wheel_modifier#", "an ankle", "a D20", "Hammond from Overwatch", 
+                            "a bacci ball", "a dung beetle pushing #roll_thing_singular#",
+                            "a wheel of parmagiano romano", "a fancy cigar", 
+                            "Louis XVI's head in the french revolution", "a coconut",
+                            "thomas the tank engine#wheel_modifier#", "a roll of toilet paper", 
+                            "a pig in the mud", "a hay bale", "that one cart wheel that refuses to cooperate but you already took the cart and there's people behind you so you're stuck with it and you should have just gotten a basket for the two things you needed but now i guess i'm gonna drag this heavy-ass hunk of metal around wegmans until the fucking rubber melts#wheel_modifier#",
+                            "an armadillo", "a pencil dropped during a math exam", 
+                            "Wario's motorbike#wheel_modifier#", "a boulder", "a can of beans",
+                            "a very round loaf of bread", "a restless baby"],
+    "wheel_modifier": ["", " with wheels made of #roll_thing_plural#"],          
+    "roll_thing_plural": ["heads in the french revolution", "thunder clouds",
+                          "cinnamon rolls", "fruit roll-ups", "rolling pins", 
+                            "tyrannosrollus rexes", "bean burritos", 
+                            "pizza rolls", "sushi rolls", "spring rolls", "tootsie rolls", 
+                            "pinewood derby cars#wheel_modifier#", "ankles", "D20s", "Hammonds from Overwatch", 
+                            "bacci balls", "dung beetles pushing #roll_thing_plural#",
+                            "wheels of parmagiano romano", "fancy cigars", 
+                            "heads in the french revolution", "coconuts",
+                            "rolls of toilet paper", "restless babies",
+                            "pigs in the mud", "hay bales", "those cart wheels that refuse to cooperate but you already took the cart and there's people behind you so you're stuck with it and you should have just gotten a basket for the two things you needed but now i guess i'm gonna drag this heavy-ass hunk of metal around wegmans until the fucking rubber melts",
+                            "armadillos", "pencils dropped during a math exam", 
+                            "Wario's motorbikes#wheel_modifier#", "boulders", "cans of beans",
+                            "very round loaves of bread"],
+    "thing_on": ["a hill", "a treadmill", "a skateboard", "rollerskates", "ecstasy", "a windmill", "a rollercoaster", "a yo-yo", "a slip 'n' slide"],
+    "thing_in": ["a rock tumbler", "a washing machine", "an industrial centrifuge", "a hurricane", 
+                 "a wind tunnel", "a cocktail shaker", "a gymnastics class", "a parkour video", 
+                 "a salad spinner", "the back of an Amazon delivery van"]
+}
+
+grammar = tracery.Grammar(rules) # create a grammar object from the rules
+grammar.add_modifiers(base_english) # add pre-programmed modifiers
+print(grammar.flatten("#lip_phrase#")) # and flatten, starting with origin rule
 
 
 
@@ -50,40 +108,49 @@ async def on_message(message):
         return
     
     # only respond in designated channel
-    if m == CHANNEL_NAME:
-        if m == 'hello':
+    if m == 'general':
+        if m == '!hello':
             await message.channel.send(f'Hello {username}!')
-            return
-        elif m == 'bye':
-            await message.channel.send(f'See you later {username}!')
-            return
-        elif m == '!random':
-            response = f'This is your random number: {random.randrange(100000)}'
-            await message.channel.send(response)
             return
     
     # if you don't check channel name, bot can respond anywhere
-    if m == '!anywhere':
-        await message.channel.send('this command can be used anywhere!')
+    if any(x in m.split() for x in ['lip', 'lipped', 'licker', '!lip']):
+        # lip_token = random.choice([t for t in m.split() if 'lip' in t]) # TODO: incorporate the word containing 'lip'
+        await message.channel.send(grammar.flatten("#lip_phrase#"))
         return
     
-    if 'lip' in m:
-        lip_token = random.choice([t for t in m.split() if 'lip' in t])
-        await message.channel.send(f'{lip_token} lipped lipid licker')
-        return
-    
-    if message_is_wordle(m):
-        await message.channel.send('ayyyyyyy its wordle!')
-        return
-    else:
-        await message.channel.send('not wordle...')
+    if 'rolled' in m:
+        await message.channel.send(grammar.flatten("#rolled#"))
         return
     
     
         
 def message_is_wordle(m):
+    # returns true if message is a pasted wordle score
     return ('wordle' in m) and re.search('[🟨🟩⬛]{5,6}', m) 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     client.run(TOKEN)
